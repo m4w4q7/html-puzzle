18770e72b2c836cf21d970c7a187ec589c13b5f2
m4w4q7 <m4w4q7@gmail.com>
2019-04-14 14:53:01 +0200

    feat(puzzle-page): add top-menu and horizontal-resizable

--- script/main.js
+++ script/main.js
@@ -1,5 +1,9 @@
+import { TopMenuComponent } from './components/top-menu/index.js';
+import { HorizontalResizableComponent } from './components/horizontal-resizable/index.js';
 import { PuzzleComponent } from './components/puzzle/index.js';
 import { PreviewComponent } from './components/preview/index.js';
 
+customElements.define('hpu-top-menu', TopMenuComponent);
+customElements.define('hpu-horizontal-resizable', HorizontalResizableComponent);
 customElements.define('hpu-puzzle', PuzzleComponent);
 customElements.define('hpu-preview', PreviewComponent);

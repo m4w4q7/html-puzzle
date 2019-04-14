7a1c51196a8c7b86ae1ed9dfb3ef0790cca53c36
m4w4q7 <m4w4q7@gmail.com>
2019-04-14 19:11:44 +0200

    feat(top-menu): use standard button for login

--- script/components/top-menu/template.html.js
+++ script/components/top-menu/template.html.js
@@ -3,7 +3,7 @@ export default `
     <div class="hpu-logo">
       <div>&lt;<span class="hpu-logo__name">html-puzzle</span>&gt;</div>
     </div>
-    <div class="hpu-login-button">
+    <div class="hpu-button">
       <div>Login</div>
     </div>
   </div>

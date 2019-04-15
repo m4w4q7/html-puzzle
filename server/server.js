2e27bd5bb2dc7f69e68842927afd854fb2b23880
m4w4q7 <m4w4q7@gmail.com>
2019-04-15 22:22:50 +0200

    feat(server): add static server

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const { join } = require('path');
+const Koa = require('koa');
+const createStaticMiddleware = require('koa-static');
+const config = require('./config');
+
+
+class Server {
+
+  constructor() {
+    this._app = new Koa();
+    this._addMiddlewares();
+  }
+
+
+  listen() {
+    this._app.listen(config.port);
+  }
+
+
+  _addMiddlewares() {
+    const staticRoot = join(__dirname, '../client');
+    this._app.use(createStaticMiddleware(staticRoot));
+  }
+
+}
+
+
+module.exports = Server;

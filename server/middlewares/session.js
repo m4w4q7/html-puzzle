6f0a4cb1573d1073552ffdbb7b524e6d3cc20d59
m4w4q7 <m4w4q7@gmail.com>
2019-05-09 21:47:29 +0200

    feat(middlewares): create session middleware

---
+++ server/middlewares/session.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+import { config } from '../config.js';
+
+
+export const sessionMiddleware = async (context, next) => {
+  try {
+    const userData = jwt.verify(context.cookies.get('auth_token'), config.authSecret);
+    context.state.user = userData;
+    await next();
+  } catch (error) {
+    console.error(error);
+    context.cookies.set('name');
+    context.cookies.set('auth_token');
+    context.status = 401;
+  }
+};

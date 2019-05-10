574147bd2b8604519816402e2b4a40eb24d9e873
m4w4q7 <m4w4q7@gmail.com>
2019-05-10 14:11:59 +0200

    fix(middlewares/session): create state if it does not exist

--- server/middlewares/session.js
+++ server/middlewares/session.js
@@ -7,7 +7,8 @@ export const sessionMiddleware = async (context, next) => {
   const authToken = context.cookies.get('auth_token');
   if (!authToken) { context.throw(401); }
   try {
-    const userData = jwt.verify(context.cookies.get('auth_token'), config.authSecret);
+    const userData = jwt.verify(authToken, config.authSecret);
+    context.state = context.state || {};
     context.state.user = userData;
   } catch (error) {
     console.error(error.toString());

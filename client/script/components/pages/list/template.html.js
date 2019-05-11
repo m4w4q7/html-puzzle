800de521e6657476f2769951f29f37dd1a589904
m4w4q7 <m4w4q7@gmail.com>
2019-05-11 14:49:17 +0200

    feat(exercises): add emptystate

--- client/script/components/pages/list/template.html.js
+++ client/script/components/pages/list/template.html.js
@@ -8,6 +8,7 @@ export default `
   <div>Exercises:</div>
   <ul>
     <li><a href="#play?exercise=dropdown">Dropdown</a></li>
+    <li><a href="#play?exercise=emptystate">Emptystate</a></li>
     <li><a href="#play?exercise=basic">Basic example to demonstrate puzzle parts</a></li>
     <li><a href="#play?exercise=text-styles">Another basic example to demonstrate previews</a></li>
   </ul>

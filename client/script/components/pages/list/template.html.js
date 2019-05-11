e8c5bc64b30d1638f355bdf7baa34705957d1e1d
m4w4q7 <m4w4q7@gmail.com>
2019-05-11 15:53:23 +0200

    feat(exercises): add complex card

--- client/script/components/pages/list/template.html.js
+++ client/script/components/pages/list/template.html.js
@@ -10,6 +10,7 @@ export default `
     <li><a href="#play?exercise=dropdown">Dropdown</a></li>
     <li><a href="#play?exercise=emptystate">Emptystate</a></li>
     <li><a href="#play?exercise=basic-card">Basic Card</a></li>
+    <li><a href="#play?exercise=complex-card">Complex Card</a></li>
     <li><a href="#play?exercise=basic">Basic example to demonstrate puzzle parts</a></li>
     <li><a href="#play?exercise=text-styles">Another basic example to demonstrate previews</a></li>
   </ul>
